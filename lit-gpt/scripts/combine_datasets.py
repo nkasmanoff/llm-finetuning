e5905c2b48bc3ffbc7f172f4da1e2ccc285b65bc
nkasmanoff <nkasmanoff@gmail.com>
2023-09-11 20:29:56 -0400

    setup for combining data

---
+++ lit-gpt/scripts/combine_datasets.py
@@ -0,0 +1,15 @@
+"""
+After downloading and processing the datasets into separate .npy files,
+we can combine them into a joint directory that allows for fine tuning over a combined dataset.
+"""
+
+import os
+
+def combine_datasets(
+    **kwargs,
+):
+    """
+    kwargs to specify which folders to use, and what proportion of each
+    """
+
+    pass
